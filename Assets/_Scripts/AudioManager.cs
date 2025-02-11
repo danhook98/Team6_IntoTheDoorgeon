8f313d78196f744719edee29989d65a47a5cecba
Dan Hook <s188873@uos.ac.uk>
2025-02-11 20:13:00 +0000

    Created the AudioManager script

---
+++ Assets/_Scripts/AudioManager.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioEventChannelSO audioEventChannel;
+    
+    [Header("Audio Sources")]
+    [SerializeField] private AudioSource sfxAudioSource;
+    [SerializeField] private AudioSource musicAudioSource;
+
+    private void Awake()
+    {
+        // Create audio sources if the references are broken or missing. 
+        if (!sfxAudioSource)
+        {
+            sfxAudioSource = gameObject.AddComponent<AudioSource>();
+        }
+
+        if (!musicAudioSource)
+        {
+            musicAudioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    /// <summary>
+    /// Plays an audio one shot of the given AudioClipSO's clip.
+    /// </summary>
+    /// <param name="audioClip">AudioClipSO data file.</param>
+    public void PlayAudioOneShot(AudioClipSO audioClip)
+    {
+        sfxAudioSource.PlayOneShot(audioClip.clip);
+    }
+
+    /// <summary>
+    /// Plays an audio clip of the given AudioClipSO's clip.
+    /// </summary>
+    /// <param name="audioClip">AudioClipSO data file.</param>
+    public void PlayMusic(AudioClipSO audioClip)
+    {
+        musicAudioSource.clip = audioClip.clip;
+        musicAudioSource.Play();
+    }
+}
