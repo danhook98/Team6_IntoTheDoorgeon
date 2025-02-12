67d4775b578a3168d74e582be85eb8ac64c5726e
Dan Hook <s188873@uos.ac.uk>
2025-02-12 18:44:29 +0000

    Update AbstractEventListener.cs

--- Assets/_Scripts/Events/AbstractEventListener.cs
+++ Assets/_Scripts/Events/AbstractEventListener.cs
@@ -3,8 +3,8 @@ using UnityEngine.Events;
 
 public abstract class AbstractEventListener<T> : MonoBehaviour
 {
-    public AbstractEvent<T> eventToListen; 
-    public UnityEvent<T> onEvent;
+    [SerializeField] private AbstractEvent<T> eventToListen; 
+    [SerializeField] private UnityEvent<T> onEvent;
 
     private void OnEnable()
     {
