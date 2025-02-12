3ffa6b42fe7e34ce0d6f4aa1dcc2aed500d191e3
NikolaosComandariu <s283743@uos.ac.uk>
2025-02-12 16:39:42 +0000

    Chooses a random door
    Picks a door from the GameObject array randomly.

--- Assets/_Scripts/DoorController.cs
+++ Assets/_Scripts/DoorController.cs
@@ -6,11 +6,17 @@ public class DoorController : MonoBehaviour
 {
     [SerializeField] private GameObject[] doors;
 
+    private void Update()
+    {
+        DecideBadDoor();
+    }
+    
     private void DecideBadDoor()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            Debug.Log(doors[Random.Range(0, doors.Length)].name);
+            var badDoor = doors[Random.Range(0, doors.Length)];
+            Debug.Log(badDoor);
         }
         
     }
