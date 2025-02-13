08dce6958abd2c1b09d9c4b7155eb594f48cc89a
s283743 <s283743@uos.ac.uk>
2025-02-13 10:25:59 +0000

    Tags bad door as BadDoor and every other door as GoodDoor

--- Assets/_Scripts/DoorController.cs
+++ Assets/_Scripts/DoorController.cs
@@ -7,56 +7,66 @@ public class DoorController : MonoBehaviour
     [SerializeField] private GameObject[] doors;
     [SerializeField] private GameObject badDoor;
 
+    private bool badDoorDecided = false;
+
     private void Update()
     {
         DecideBadDoor();
-        CheckPlayerChoice();
+        //CheckPlayerChoice();
     }
-    
-    private void DecideBadDoor()
+
+    public void DecideBadDoor()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E)) // TODO: if(badDoorDecided = false) & Remove this if statement for testing.
         {
             badDoor = doors[Random.Range(0, doors.Length)];
+            
+            for(var i = 0; i < doors.Length; i++)
+                doors[i].tag = "GoodDoor";
+            
             badDoor.tag = "BadDoor";
-            
+            badDoorDecided = true;
+
             Debug.Log(badDoor);
-        }
-    }
-
-    private void CheckPlayerChoice()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down);
-            
-            switch (hit.collider.gameObject.tag)
-            {
-                case "BadDoor":
-                    Debug.Log("Player has chosen the bad door");
-                    break;
-                default:
-                    Debug.Log(hit.collider.gameObject.name);
-                    break;
-            }
+            // TODO: IF player picks bad door or goes into new room, badDoorDecided = false;
         }
-    }
-    
-    private void BadDoor()
-    {
-        // TODO: Reset current score to 0
-        // Make death screen visible
-        // Play BadDoor SFX and bad door animation
-        Debug.Log("Player clicked on bad door!");
-    }
-
-    private void GoodDoor()
-    {
-        // TODO: Add score to the current score
-        // Allow the player to make a new choice
-        // Play GoodDoor SFX and bad door animation
-        Debug.Log("Player clicked on good door!");
     }
 }
+
+/* private void CheckPlayerChoice()
+ {
+     if (Input.GetMouseButtonDown(0))
+     {
+         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down);
+
+         switch (hit.collider.gameObject.tag)
+         {
+             case "BadDoor":
+                 Debug.Log("Player has chosen the bad door");
+                 break;
+             default:
+                 Debug.Log(hit.collider.gameObject.name);
+                 break;
+         }
+     }
+ }
+
+ private void BadDoor()
+ {
+     // TODO: Reset current score to 0
+     // Make death screen visible
+     // Play BadDoor SFX and bad door animation
+     Debug.Log("Player clicked on bad door!");
+ }
+
+ private void GoodDoor()
+ {
+     // TODO: Add score to the current score
+     // Allow the player to make a new choice
+     // Play GoodDoor SFX and bad door animation
+     Debug.Log("Player clicked on good door!");
+ }
+} */
