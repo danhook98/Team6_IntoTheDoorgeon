049b6c1d1b26a1d566d7f292432c301676d987c8
danhook98 <s188873@uos.ac.uk>
2025-02-13 10:26:18 +0000

    Added basic support for score

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -1,9 +1,33 @@
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace DoorGame
 {
     public class GameController : MonoBehaviour
     {
+        [Header("Score Variables")] 
+        [SerializeField] private int minimumScoreToAdd = 15;
+        [SerializeField] private int maxScoreToAdd = 45;
         
+        // reference to the door manager
+        private int _score;
+        private int _highScore;
+
+        private void Awake()
+        {
+            _score = 0; 
+            _highScore = PlayerPrefs.GetInt("HighScore", 0);
+        }
+
+        private void Start()
+        {
+            // tell the door manager to generate doors
+        }
+        
+        // Score
+        public void AddScore()
+        {
+            _score += Random.Range(minimumScoreToAdd, maxScoreToAdd + 1);
+        }
     }
 }
