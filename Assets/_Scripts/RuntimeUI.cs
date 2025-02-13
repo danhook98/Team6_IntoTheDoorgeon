f4d151a652dcaa41eea77d9a4b6263a580e45cf3
s283743 <s283743@uos.ac.uk>
2025-02-13 14:24:12 +0000

    RuntimeUI.cs + Main Canvas work

--- Assets/_Scripts/RuntimeUI.cs
+++ Assets/_Scripts/RuntimeUI.cs
@@ -1,21 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DoorGame.Events;
+using UnityEngine.Serialization;
 
 namespace DoorGame
 {
     public class RuntimeUI : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
+        [Header("Events")]
+        [SerializeField] private BoolEvent onPauseGameEvent;
+        [SerializeField] private BoolEvent onResumeGameEvent;
+        [SerializeField] private BoolEvent onLoadMainGameEvent;
+        [SerializeField] private BoolEvent onRestartCurrentSceneEvent;
 
-        // Update is called once per frame
         void Update()
         {
+            Debug.Log("Test");
+        }
         
+        public void PauseGame()
+        {
+            Debug.Log("Game paused");
+            onPauseGameEvent.Invoke(true);
+        }
+
+        public void ResumeGame()
+        {
+            Debug.Log("Game resumed");
+            onResumeGameEvent.Invoke(true);
+        }
+
+        public void LoadMainGameScene()
+        {
+            //SceneManager.LoadScene("Game"); //TODO: Uncomment this so it works.
+        }
+
+        public void RestartGame()
+        {
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            //onRestartCurrentSceneEvent.Invoke();
+        }
+
+        public void LoadMainMenuScene()
+        {
+            //SceneManager.LoadScene("MainMenu"); //TODO: Uncomment this so it works.
         }
     }
 }
