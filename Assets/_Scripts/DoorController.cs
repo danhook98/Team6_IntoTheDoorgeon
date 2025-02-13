296b14cd1092d60c2df0aefdb99a747a5fab63b0
s283743 <s283743@uos.ac.uk>
2025-02-13 10:46:14 +0000

    Commented DoorController.cs

--- Assets/_Scripts/DoorController.cs
+++ Assets/_Scripts/DoorController.cs
@@ -11,10 +11,15 @@ public class DoorController : MonoBehaviour
 
     public void DecideBadDoor()
     {
+        // Resets bad door selected to false so this can be called/used again.
         badDoorDecided = false;
         
         if (badDoorDecided == false)
         {
+            // If bad door has not been selected, pick a door randomly from the array
+            // and tag every game object in the array but the bad door as "GoodDoor"
+            // Bad door labelled as "BadDoor". 
+            
             badDoor = doors[Random.Range(0, doors.Length)];
             
             for(var i = 0; i < doors.Length; i++)
