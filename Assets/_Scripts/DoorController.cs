b2ccc6b8e656aa6b80cc9e89431dab2a4fa594e3
s283743 <s283743@uos.ac.uk>
2025-02-13 10:38:36 +0000

    Updated DoorController

--- Assets/_Scripts/DoorController.cs
+++ Assets/_Scripts/DoorController.cs
@@ -9,14 +9,10 @@ public class DoorController : MonoBehaviour
 
     private bool badDoorDecided = false;
 
-    private void Update()
-    {
-        DecideBadDoor();
-        //CheckPlayerChoice();
-    }
-
     public void DecideBadDoor()
     {
+        badDoorDecided = false;
+        
         if (badDoorDecided == false)
         {
             badDoor = doors[Random.Range(0, doors.Length)];
@@ -32,40 +28,3 @@ public class DoorController : MonoBehaviour
         } 
     }
 }
-
-/* private void CheckPlayerChoice()
- {
-     if (Input.GetMouseButtonDown(0))
-     {
-         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down);
-
-         switch (hit.collider.gameObject.tag)
-         {
-             case "BadDoor":
-                 Debug.Log("Player has chosen the bad door");
-                 break;
-             default:
-                 Debug.Log(hit.collider.gameObject.name);
-                 break;
-         }
-     }
- }
-
- private void BadDoor()
- {
-     // TODO: Reset current score to 0
-     // Make death screen visible
-     // Play BadDoor SFX and bad door animation
-     Debug.Log("Player clicked on bad door!");
- }
-
- private void GoodDoor()
- {
-     // TODO: Add score to the current score
-     // Allow the player to make a new choice
-     // Play GoodDoor SFX and bad door animation
-     Debug.Log("Player clicked on good door!");
- }
-} */
