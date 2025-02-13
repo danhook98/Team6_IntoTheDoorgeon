3f4dad4d98940e296d30f7f40f0d3f8db9db1bcc
danhook98 <s188873@uos.ac.uk>
2025-02-13 11:10:51 +0000

    Refactored DoorController.cs

--- Assets/_Scripts/DoorController.cs
+++ Assets/_Scripts/DoorController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace DoorGame
 {
@@ -7,30 +8,24 @@ namespace DoorGame
         [SerializeField] private GameObject[] doors;
         [SerializeField] private GameObject badDoor;
 
-        private bool badDoorDecided = false;
-
-        public void DecideBadDoor()
+        public void GenerateDoors()
         {
-            // Resets bad door selected to false so this can be called/used again.
-            badDoorDecided = false;
-        
-            if (badDoorDecided == false)
-            {
-                // If bad door has not been selected, pick a door randomly from the array
-                // and tag every game object in the array but the bad door as "GoodDoor"
-                // Bad door labelled as "BadDoor". 
-            
-                badDoor = doors[Random.Range(0, doors.Length)];
-            
-                for(var i = 0; i < doors.Length; i++)
-                    doors[i].tag = "GoodDoor";
-            
-                badDoor.tag = "BadDoor";
-            
+            // If bad door has not been selected, pick a door randomly from the array
+            // and tag every game object in the array but the bad door as "GoodDoor"
+            // Bad door labelled as "BadDoor". 
 
-                Debug.Log(badDoor);
-                badDoorDecided = true;
-            } 
+            ResetDoors();
+
+            badDoor = doors[Random.Range(0, doors.Length)];
+            badDoor.tag = "BadDoor";
+
+            // Debug.Log(badDoor);
+        }
+
+        private void ResetDoors()
+        {
+            foreach (var door in doors)
+                door.tag = "GoodDoor";
         }
     }
 }
