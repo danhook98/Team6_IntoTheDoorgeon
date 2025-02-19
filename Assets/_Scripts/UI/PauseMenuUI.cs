a6e32d5023faf9168aec33fb63e14e92fc2e0a6c
NikolaosComandariu <s283743@uos.ac.uk>
2025-02-19 11:40:20 +0000

    Functions to turn the win and lose screens on implemented

--- Assets/_Scripts/UI/PauseMenuUI.cs
+++ Assets/_Scripts/UI/PauseMenuUI.cs
@@ -6,9 +6,12 @@ namespace DoorGame.UI
 {
     public class PauseMenuUI : MonoBehaviour
     {
+        [Header("Game Objects")]
         [SerializeField] private Canvas pauseMenuCanvas;
         [SerializeField] private GameObject pauseMenuContainer;
         [SerializeField] private GameObject optionsMenuContainer;
+        [SerializeField] private GameObject winMenuContainer;
+        [SerializeField] private GameObject loseMenuContainer;
         [SerializeField] private GameObject audioManager;
         
         [Header("Events")]
@@ -43,6 +46,16 @@ namespace DoorGame.UI
         {
             optionsMenuContainer.SetActive(false);
             pauseMenuContainer.SetActive(true);
+        }
+
+        public void WinScreenOn()
+        {
+            winMenuContainer.SetActive(true);
+        }
+
+        public void LoseScreenOn()
+        {
+            loseMenuContainer.SetActive(true);
         }
         
         //Audio Settings
