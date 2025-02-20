4c39ee2a70e6d8180c9500ba907f54ab77f2ef13
Dan Hook <s188873@uos.ac.uk>
2025-02-20 13:23:01 +0000

    Fixed doubled pause sound playing

--- Assets/_Scripts/UI/PauseMenuUI.cs
+++ Assets/_Scripts/UI/PauseMenuUI.cs
@@ -29,16 +29,12 @@ namespace DoorGame.UI
         {
             Debug.Log(isPaused);
             pauseMenuCanvas.enabled = isPaused;
-            playSfxAudioChannel.Invoke(pauseSound);
+            playSfxAudioChannel.Invoke(isPaused ? pauseSound : unpauseSound);
         }
         
         // Pause menu button actions. 
-        public void ResumeGame()
-        {
-            onPauseGameEvent.Invoke(false); // False = not paused.
-            playSfxAudioChannel.Invoke(unpauseSound);
-        } 
-        
+        public void ResumeGame() => onPauseGameEvent.Invoke(false); // False = not paused.
+
         public void RestartGame() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         public void OpenOptionsMenu()
         {
