eb697aaf466964311128ac1c8215f13e542f740c
NikolaosComandariu <s283743@uos.ac.uk>
2025-02-24 15:25:49 +0000

    Updated MainMenuController

--- Assets/_Scripts/UI/MainMenuController.cs
+++ Assets/_Scripts/UI/MainMenuController.cs
@@ -7,6 +7,8 @@ namespace DoorGame
 {
     public class MainMenuController : MonoBehaviour
     {
+        [SerializeField] private GameObject optionsMenuContainer;
+        [SerializeField] private GameObject mainMenuContainer;
         public void PlayGame()
         {
             SceneManager.LoadSceneAsync("Main Game");
@@ -15,6 +17,18 @@ namespace DoorGame
         public void QuitGame()
         {
             Application.Quit();
+        }
+
+        public void OpenOptionsMenu()
+        {
+            optionsMenuContainer.SetActive(true);
+            mainMenuContainer.SetActive(false);
+        }
+
+        public void CloseOptionsMenu()
+        {
+            optionsMenuContainer.SetActive(false);
+            mainMenuContainer.SetActive(true);
         }
     }
 }
