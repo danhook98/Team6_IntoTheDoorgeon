495cf454abbe0ddea083a4fb2549e006270135c6
NikolaosComandariu <s283743@uos.ac.uk>
2025-06-10 21:16:14 +0100

    Implement resolution settings

--- Assets/_Scripts/Settings.cs
+++ Assets/_Scripts/Settings.cs
@@ -1,21 +1,56 @@
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using TMPro;
 
 namespace DoorGame
 {
     public class Settings : MonoBehaviour
     {
+        // Contrast & Brightness Related.
         private Volume _postProcessVolume;
         private ColorAdjustments _colorAdjustments;
+        
+        // Resolution Related.
+        private Resolution[] _resolutions;
+        [SerializeField] private TMP_Dropdown resolutionDropdown;
         
         private void Awake()
         {
             _postProcessVolume = GameObject.Find("Global Volume").GetComponent<Volume>();
+            resolutionDropdown = GameObject.Find("Resolution Dropdown").GetComponent<TMP_Dropdown>();
             _postProcessVolume.profile.TryGet<ColorAdjustments>(out _colorAdjustments);
 
+            // Load PlayerPrefs
             AdjustBrightness(PlayerPrefs.GetFloat("Brightness"));
             AdjustContrast(PlayerPrefs.GetFloat("Contrast"));
+            if(PlayerPrefs.GetInt("Fullscreen") == 1) SetFullscreen(true);
+            else SetFullscreen(false);
+        }
+
+        private void Start()
+        {
+            _resolutions = Screen.resolutions;
+            resolutionDropdown.ClearOptions();
+
+            List<string> options = new List<string>();
+
+            int currentResolutionIndex = 0;
+            for (int i = 0; i < _resolutions.Length; i++)
+            {
+                string option = _resolutions[i].width + " x " + _resolutions[i].height;
+                options.Add(option);
+
+                if (_resolutions[i].width == Screen.width && _resolutions[i].height == Screen.height)
+                {
+                    currentResolutionIndex = i;
+                }
+            }
+            resolutionDropdown.AddOptions(options);
+            resolutionDropdown.value = currentResolutionIndex;
+            resolutionDropdown.RefreshShownValue();
         }
         
         public void AdjustBrightness(float value)
@@ -33,6 +68,12 @@ namespace DoorGame
         public void SetFullscreen(bool isFullscreen)
         {
             Screen.fullScreen = isFullscreen;
+            PlayerPrefs.SetInt("Fullscreen", isFullscreen ? 1 : 0);
+        }
+
+        public void SetResolution(int resolutionIndex)
+        {
+            Screen.SetResolution(_resolutions[resolutionIndex].width, _resolutions[resolutionIndex].height, Screen.fullScreen);
         }
     }
 }
