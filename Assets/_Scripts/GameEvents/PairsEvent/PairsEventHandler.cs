0288a1f57df22e3facd751c8f9718a9492b261ec
NikolaosComandariu <s283743@uos.ac.uk>
2025-06-11 17:38:27 +0100

    Create 12 prefabs & add to list

--- Assets/_Scripts/GameEvents/PairsEvent/PairsEventHandler.cs
+++ Assets/_Scripts/GameEvents/PairsEvent/PairsEventHandler.cs
@@ -7,30 +7,32 @@ namespace DoorGame.GameEvents.PairsEvent
     public class PairsEventHandler : MonoBehaviour
     {
         // Add new list with 12 card prefabs. Then select 8 of these and place into available cards.
-        [SerializeField] private List<GameObject> _availableCards;
-        [SerializeField] private List<GameObject> _usedCards;
-        [SerializeField] private int _attempts;
-        [SerializeField] private List<Vector2> _spawnPositionsAvailable;
-        [SerializeField] private List<Vector2> _spawnPositionsUsed;
+        [SerializeField] private List<GameObject> cards;
+        [SerializeField] private List<GameObject> availableCardPairs;
+        [SerializeField] private List<GameObject> usedCards;
+        
+        [SerializeField] private List<Vector2> spawnPositionsAvailable;
+        [SerializeField] private List<Vector2> spawnPositionsUsed;
+        
+        [SerializeField] private int attempts;
         
         private int _completedPairs;
-        private int _totalPairs;
+        private const int TotalPairs = 8;
 
         private void Start()
         {
-            _attempts = 5;
+            attempts = 5;
             //SpawnCards();
         }
 
         private void SpawnCards()
         {
-            _totalPairs = _availableCards.Count;
-            for (int i = 0; i < _totalPairs; i++)
+            for (int i = 0; i < TotalPairs; i++)
             {
-                int randomSpawnPoint = Random.Range(0, _spawnPositionsAvailable.Count);
-                Vector3 spawnPosition = _spawnPositionsAvailable[randomSpawnPoint];
-                _spawnPositionsUsed.Add(spawnPosition); 
-                var cardID = Random.Range(0, _availableCards.Count);
+                int randomSpawnPoint = Random.Range(0, spawnPositionsAvailable.Count);
+                Vector3 spawnPosition = spawnPositionsAvailable[randomSpawnPoint];
+                spawnPositionsUsed.Add(spawnPosition); 
+                var cardID = Random.Range(0, availableCardPairs.Count);
                 //Instantiate(_availableCards(cardID), spawnPosition, Quaternion.identity);
                // _availableCards.RemoveAt(cardID);
             }
