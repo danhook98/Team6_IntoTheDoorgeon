fcbdfe2718f10086b0c06d88485b47878654f452
NikolaosComandariu <s283743@uos.ac.uk>
2025-06-11 16:47:31 +0100

    Started working on card spawn system

---
+++ Assets/_Scripts/GameEvents/PairsEvent/PairsEventHandler.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace DoorGame
+{
+    public class PairsEventHandler : MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> _availableCards;
+        [SerializeField] private List<GameObject> _usedCards;
+        [SerializeField] private int _lives;
+        [SerializeField] private List<Vector2> _spawnPositionsAvailable;
+        [SerializeField] private List<Vector2> _spawnPositionsUsed;
+        
+        private int _completedPairs;
+        private int _totalPairs;
+
+        private void Start()
+        {
+            _lives = 3;
+            SpawnCards();
+        }
+
+        private void SpawnCards()
+        {
+            _totalPairs = _availableCards.Count;
+            for (int i = 0; i < _totalPairs; i++)
+            {
+                int randomSpawnPoint = Random.Range(0, _spawnPositionsAvailable.Count);
+                Vector3 spawnPosition = _spawnPositionsAvailable[randomSpawnPoint];
+                _spawnPositionsUsed.Add(spawnPosition); 
+                var cardID = Random.Range(0, _availableCards.Count);
+                //Instantiate(_availableCards(cardID), spawnPosition, Quaternion.identity);
+               // _availableCards.RemoveAt(cardID);
+            }
+        }
+    }
+}
