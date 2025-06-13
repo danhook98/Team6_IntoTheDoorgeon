a522d9dbc8fa3372ba3865e0b6b46d549f65a89b
Dan Hook <s188873@uos.ac.uk>
2025-06-13 17:31:06 +0100

    Add primitive wheel spin

--- Assets/_Scripts/GameplayEvents/WheelOfFortune.cs
+++ Assets/_Scripts/GameplayEvents/WheelOfFortune.cs
@@ -1,21 +1,45 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-namespace DoorGame
+namespace DoorGame.GameplayEvents
 {
     public class WheelOfFortune : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        [Header("Wheel Variables")]
+        [SerializeField] private float initialTorquePower = 1000f;
         
+        private Rigidbody2D _rigidbody2D;
+        private WaitForSeconds _spinCheckDelay;
+
+        private void Awake()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+            _spinCheckDelay = new WaitForSeconds(0.1f);
         }
 
-        // Update is called once per frame
-        void Update()
+        private void Update()
         {
-        
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                StartCoroutine(Spin());
+            }
+        }
+
+        private IEnumerator Spin()
+        {
+            _rigidbody2D.AddTorque(initialTorquePower);
+
+            yield return _spinCheckDelay;
+
+            while (_rigidbody2D.angularVelocity > 0f)
+            {
+                Debug.Log("Reducing angular torque");
+                _rigidbody2D.angularVelocity -= 200 * Time.deltaTime;
+                yield return null; 
+            }
+            
+            // Get results.
         }
     }
 }
