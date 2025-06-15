243f53de0dcf4c3341fb5e5a0db152840960ba3c
NikolaosComandariu <s283743@uos.ac.uk>
2025-06-15 14:44:21 +0100

    Assign a random tag to mysterious door

--- Assets/_Scripts/GameEvents/MagicalDoorEvent/MagicalDoorHandler.cs
+++ Assets/_Scripts/GameEvents/MagicalDoorEvent/MagicalDoorHandler.cs
@@ -1,21 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace DoorGame
 {
     public class MagicalDoorHandler : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        private string[] _tags;
+        private GameObject _mysteriousDoor;
+
+        private void Start()
         {
-        
+            _mysteriousDoor = this.gameObject;
+            _tags = new string[] { "MagicalDoor", "CursedDoor" };
         }
 
-        // Update is called once per frame
-        void Update()
+        private void Update()
         {
-        
+            if (Input.GetKeyDown(KeyCode.T)) // TODO: remove after testing.
+            {
+                SetTag();
+            }
+        }
+
+        private void SetTag()
+        {
+            _mysteriousDoor.tag = _tags[Random.Range(0, 2)];
         }
     }
 }
