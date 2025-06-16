f7fd8c9f0f11b390d97756fc71308792d044f736
NikolaosComandariu <s283743@uos.ac.uk>
2025-06-16 20:16:13 +0100

    Check if first bad door can spawn

--- Assets/_Scripts/Door/DoorController.cs
+++ Assets/_Scripts/Door/DoorController.cs
@@ -10,16 +10,30 @@ namespace DoorGame.Door
         [SerializeField] private VoidEvent onGoodDoorOpenedEvent; 
         [SerializeField] private VoidEvent onBadDoorOpenedEvent;
         
+        [Header("Doors")]
         [SerializeField] private Door[] doors;
         
         private Door _badDoor;
+        
+        // Bad door chances.
+        private int _oneBadDoorChance;
+        private int _twoBadDoorChance;
 
+        private void Start()
+        {
+            _oneBadDoorChance = 0;
+        }
+        
         public void GenerateDoors()
         {
             // If bad door has not been selected, pick a door randomly from the array and tag every game object in the
             // array but the bad door as "GoodDoor", bad door labelled as "BadDoor". 
             ResetDoors();
             
+            // Check if bad door can be spawned.
+            var thresholdForBadDoor = Random.Range(0, 101);
+            if (thresholdForBadDoor > _oneBadDoorChance) return;
+                
             _badDoor = doors[Random.Range(0, doors.Length)];
             _badDoor.tag = "BadDoor";
         }
@@ -47,6 +61,19 @@ namespace DoorGame.Door
                 onGoodDoorOpenedEvent.Invoke(new Empty());
                 Debug.Log("Good door opened", this);
             }
+        }
+
+        public void IncreaseBadDoorChance()
+        {
+            _oneBadDoorChance += 10;
+
+            if (_oneBadDoorChance >= 100)
+            {
+                _oneBadDoorChance = 100;
+                _twoBadDoorChance += 10;
+            }
+            
+            if(_twoBadDoorChance >= 50) _twoBadDoorChance = 50;
         }
     }
 }
