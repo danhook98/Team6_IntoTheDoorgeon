afe8959c8edaa500aa12549850edfa9da1539dc0
NikolaosComandariu <s283743@uos.ac.uk>
2025-06-18 17:15:42 +0100

    Update mysterious door end card text

--- Assets/_Scripts/GameplayEvents/MagicalDoorEvent/MagicalDoorHandler.cs
+++ Assets/_Scripts/GameplayEvents/MagicalDoorEvent/MagicalDoorHandler.cs
@@ -1,18 +1,29 @@
-using System;
 using UnityEngine;
 using Random = UnityEngine.Random;
+using TMPro;
 
 namespace DoorGame.GameplayEvents.MagicalDoorEvent
 {
     public class MagicalDoorHandler : MonoBehaviour
     {
+        [SerializeField] private GameObject mysteriousDoor;
+        [SerializeField] private TextMeshProUGUI text;
+        
         private string[] _tags = { "MagicalDoor", "CursedDoor" };
 
         private void OnEnable() => SetTag();
 
         public void SetTag()
         {
-            gameObject.tag = _tags[Random.Range(0, _tags.Length)];
+            mysteriousDoor.tag = _tags[Random.Range(0, _tags.Length)];
+            if (mysteriousDoor.CompareTag("CursedDoor"))
+            {
+                text.text = "You opened a cursed door!\n" + "You will only gain half the points in the next room!";
+            }
+            else
+            {
+                text.text = "You opened a magical door!\n" + "You will gain double the points in the next room!";
+            }
         }
     }
 }
